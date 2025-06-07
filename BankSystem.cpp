a7afe462c5c7bb88aeb0bceccfc0556916b11a7c
Anna Astvatsatryan <astvanna@gmail.com>
2025-06-07 21:54:35 +0400

    Implementation of Bank System class

---
+++ BankSystem.cpp
@@ -0,0 +1,45 @@
+#include "BankSystem.h"
+#include <iostream>
+
+void BankSystem::addAccount(const std::string &accHolder, int accNum, double initBalance) {
+	for (const auto &account : accounts) {
+		if (account.getAccountNumber() == accNum) {
+			std::cout << "Account number " << accNum << " already exists." << std::endl;
+			return;
+		}
+	}
+	
+	accounts.emplace_back(BankAccount(accHolder, accNum, initBalance));
+}
+
+BankAccount* BankSystem::findAccount(int accNum) {
+	for (auto &account : accounts) {
+		if (account.getAccountNumber() == accNum) {
+			return &account;
+		}
+	}
+	std::cout << "Account " << accNum << " not found." << std::endl;
+	return nullptr;
+}
+
+void BankSystem::transferFunds(int senderAccNum, int receiverAccNum, double amount) {
+	BankAccount* sender = findAccount(senderAccNum);
+	BankAccount* receiver = findAccount(receiverAccNum);
+	
+	if (sender && receiver) {
+		if (sender->withdraw(amount)) {
+			receiver->deposit(amount);
+			std::cout << "Transferred " << amount << "$ from account " << senderAccNum 
+					  << " to account " << receiverAccNum << std::endl;
+		}
+	} else {
+		std::cout << "Transfer failed. 0 accounts found." << std::endl;
+	}
+}
+
+void BankSystem::displayAllAccounts() const {
+	std::cout << "\nAll Bank Accounts" << std::endl;
+	for (const auto &account : accounts) {
+		account.display();
+	}
+}
